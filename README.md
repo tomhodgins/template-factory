963bb0e474182af0e4fefbdabfac7eab69210970
Tom Hodgins <tomhodgins@gmail.com>
2016-01-11 22:03:30 -0500

    Add color themes to tooltips

--- README.md
+++ README.md
@@ -28,12 +28,12 @@ The second type of thing being developed at the Template Factory are CSS and Jav
 Included plugins are:
 
 - **[EQCSS.js](http://elementqueries.com)** for Element Queries and scoped CSS
-- [responsive video scaling](http://staticresource.com/template/plugins/video-scaling)
-- [tooltips](http://staticresource.com/template/plugins/tooltips)
-- [modals](http://staticresource.com/template/plugins/modals)
-- [notifications](http://staticresource.com/template/plugins/notifications)
 - [responsive order form](http://staticresource.com/template/plugins/form)
+- [responsive video scaling](http://staticresource.com/template/plugins/video-scaling)
+- [notifications](http://staticresource.com/template/plugins/notifications)
+- [tooltips](http://staticresource.com/template/plugins/tooltips)
 - [responsive tables](http://staticresource.com/template/plugins/tables)
+- [modals](http://staticresource.com/template/plugins/modals)
 - [CSS buttons](http://staticresource.com/template/plugins/buttons)
 
 ## How to build a website with the Template Factory
@@ -44,3 +44,32 @@ Included plugins are:
 ## To create new pages
 
 - create new `.php` files in the main folder
+- set the `$template`, `$template_title`, and `$template_theme` variables at the top of the page
+- include header and footer partials
+- add your content.
+
+All in all, a new empty template file might look like:
+
+```
+<?php
+  $template = 'single';
+  $template_title = 'The Template Factory';
+  $template_theme = 'dark';
+?>
+<?php include("inc/header.php"); ?>
+<h1><?php echo $template_title; ?></h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+<?php include("inc/footer.php"); ?>
+```
+
+## To define a new modal
+
+- Create a new `.php` file in `inc/modals` with your desired content
+- set the `$modal` variable to the same name as the filename for your modal content. `inc/modals/example.php` would be `$modal = "example";`
+- include the modal plugin on your page with `<?php include("plugins/modals/modal.php"); ?>`
+- launch the modal by name with the JavaScript `spawnModal('')` and supply the same name as the file, e.g. `spawnModal('example')`
+
+```
+<input onclick=spanwmodal('demo')
+<?php $modal = "demo"; include("plugins/modals/modal.php"); ?>
+```
--- index.php
+++ index.php
@@ -6,18 +6,27 @@
 <?php include("inc/header.php"); ?>
 <h1>The Template Factory</h1>
 <h2>Testing various themes and plugins against each other</h2>
-<p>The goal of this site is to provide a simple way to build and test new code, and improve existing code.</p>
-<h3>Theme Demos</h3>
+<p>The Template Factory is a small PHP-based website template written simply in PHP, HTML, CSS and JavaScript. Powered by EQCSS for element queries, plus lightweight plugins for tooltips, modals, notifications, and more!</p>
+<h2>What does it do?</h2>
+<p>The Template Factory is home to a few different things that are being developed, first there are a number of basic CSS styles being developed and tested so they can be quickly applied to new sites or new content on existing sites without having to worry about creating default styles. These themes can be tested with dummy content using their theme pages below:</p>
+<h3>Themes</h3>
+<p>Included themes from <code>basic.css</code> are:</p>
 <ul>
   <li><a href=themes/default.php data-button>Default Theme</a>
   <li><a href=themes/dark.php data-button=green>Dark Theme</a>
   <li><a href=themes/code.php data-button=blue>Code Theme</a>
   <li><a href=themes/book.php data-button=red>Book Theme</a>
+</ul>
+<h3>Styles</h3>
+<p>And via additional stylesheets there are also:</p>
+<ul>
   <li><a href=themes/green.php data-button>Green style</a>
   <li><a href=themes/swiss.php data-button=blue>Swiss style</a>
   <li><a href=themes/rfi.php data-button=green>RFI Style</a>
 </ul>
-<h3>Plugin Tests</h3>
+<p>The second type of thing being developed at the Template Factory are CSS and JavaScript plugins that are intended to be used across multiple sites. What better environment to test these plugins than in a multi-themed website. These plugins are all located inside their own folders in the <code>plugins/</code> folder, often with their own documentation labelled as <code>index.php</code>.</p>
+<h3>Plugins</h3>
+<p>Included plugins are:</p>
 <ul>
   <li><a href=plugins/form data-button>Order Form</a>
   <li><a href=plugins/video-scaling data-button>Video Scaling</a>
@@ -27,5 +36,37 @@
   <li><a href=plugins/modals data-button>Modals</a>
   <li><a href=plugins/buttons data-button>Buttons</a>
 </ul>
+<h2>Using the Template Factory</h2>
+<h3>How to build a website with the Template Factory</h3>
+<ul>
+  <li>clone this repository from Github
+  <li>change the <code>$site</code> variable at the top of <code>inc/header.php</code> to the address where you host this repository
+</ul>
+<h3>To create new pages</h3>
+<ul>
+  <li>create new <code>.php</code> files in the main folder
+  <li>set the <code>$template</code>, <code>$template_title</code>, and <code>$template_theme</code> variables at the top of the page
+  <li>include header and footer partials with <code>&lt;php include("inc/header.php"); ?></code> and <code>&lt;php include("inc/footer.php"); ?></code>
+  <li>add your content
+</ul>
+<p>All in all, a new empty template file might look like:</p>
+<pre style=white-space:pre>&lt;?php
+  $template = 'single';
+  $template_title = 'The Template Factory';
+  $template_theme = 'dark';
+?>
+&lt;?php include("inc/header.php"); ?>
+&lt;h1><?php echo $template_title; ?>&lt;/h1>
+&lt;p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.&lt;/p>
+&lt;?php include("inc/footer.php"); ?></pre>
+<h3>To define a new modal</h3>
+<ul>
+  <li>create a new <code>.php</code> file in <code>inc/modals</code> with your desired content
+  <li>set the <code>$modal</code> variable to the same name as the filename for your modal content. <code>inc/modals/example.php</code> would be <code>$modal = "example";</code>
+  <li>include the modal plugin on your page with <code>&lt;?php include("plugins/modals/modal.php"); ?></code>
+  <li>launch the modal by name with the JavaScript <code>spawnModal('')</code> and supply the same name as the file, e.g. <code>spawnModal('example')</code>
+</ul>
+<pre style=white-space:pre>&lt;input onclick=spanwmodal('demo')
+&lt;?php $modal = "demo"; include("plugins/modals/modal.php") ?></pre>
 <footer>&copy; <script>document.write(new Date().getFullYear())</script> Tommy Hodgins</footer>
 <?php include("inc/footer.php"); ?>
